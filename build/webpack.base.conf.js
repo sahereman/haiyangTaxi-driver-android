57a54bf351ca0390dd2786ef3c0c524a4e0c3c50
hmaisky <lhm17863972036@163.com>
2019-01-11 17:58:18 +0800

    vue添加mp3配置

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -61,6 +61,13 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.(mp3)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          name: utils.assetsPath('assets/[name].[hash:7].[ext]')
+        }
       }
     ]
   }
